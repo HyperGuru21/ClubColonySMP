df0992c34679a0dab125f5c71880471d8fe9c00c
HyperGuru21 <94612217+HyperGuru21@users.noreply.github.com>
2021-11-21 22:01:30 +0530

    Add files via upload

---
+++ code/main.js
@@ -0,0 +1,10 @@
+function play(){
+        document.getElementById("audio1").play();
+}
+function changecolour(){
+        document.getElementById("para1").style.color="red";
+}
+function changecolor(){
+        document.getElementById("h3").style.color="white";
+}
+console.log('My website');
